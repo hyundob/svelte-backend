a76f3314ce35bd74bd70847011264accb11986c3
Song Do Hyeon <fsxsong@gmail.com>
2025-07-16 09:57:54 +0900

    2025-07-16 setting

--- src/main/java/com/example/template_demo/entity/Member.java
+++ src/main/java/com/example/template_demo/entity/Member.java
@@ -1,4 +1,19 @@
 package com.example.template_demo.entity;
 
-public class member {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    //Getter and Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //Field
+    private Integer Idx;
+
+    private String id;
+    private String pw;
 }
--- src/main/java/com/example/template_demo/entity/MemberRepository.java
+++ src/main/java/com/example/template_demo/entity/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.example.template_demo.entity;
 
-public interface MemberRepository {
+import com.example.template_demo.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findById(String id);
 }
